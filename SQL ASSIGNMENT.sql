367d7438449a4434d63809d2d980c7661064dabb
francissuccess <81147087+francissuccess@users.noreply.github.com>
2023-09-04 16:26:23 +0100

    SQL ASSIGNMENT

---
+++ SQL ASSIGNMENT.sql
@@ -0,0 +1,15 @@
+create Table Customer_Database (
+CustomerID int Primary key,
+    FirstName varchar(255),
+    LastName varchar(255),
+    Address varchar(255),
+    Dateofbirth date
+	);
+	Alter table Customer_Database
+	Add Phonenum Varchar(255)
+
+
+
+Insert into Customer_Database(CustomerID,Firstname,Lastname,Address,Dateofbirth,Phonenum)
+	Values(123459,'James','Success','23 Abuleegba Puposola Lagos','09/06/1990', '09020920490')
+	Select * from Customer_Database
